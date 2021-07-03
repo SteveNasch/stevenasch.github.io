d76ae965404a8f35cedfde07340c8f1f1f7bc87c
Estêvão Sievert Naschenweng <estevao.nasch@gmail.com>
2021-07-03 14:07:53 -0300

    Incluindo JS

---
+++ funcoes.js
@@ -0,0 +1,10 @@
+  function voltar(){
+    var botao = document.getElementById("btvoltar")
+    botao.className = "btn btn-danger";    
+  }
+
+  function mostrarjanela(document){
+    document.getElementById('download').style.display='block';
+  }
+
+  
