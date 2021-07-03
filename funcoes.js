41a2c04d5f35ab72305963f3d2145833f46597b8
Estêvão Sievert Naschenweng <estevao.nasch@gmail.com>
2021-07-03 14:22:24 -0300

    Corrigindo bugs

--- funcoes.js
+++ funcoes.js
@@ -1,7 +1,3 @@
-  function voltar(){
-    var botao = document.getElementById("btvoltar")
-    botao.className = "btn btn-danger";    
-  }
 
   function mostrarjanela(document){
     document.getElementById('download').style.display='block';
